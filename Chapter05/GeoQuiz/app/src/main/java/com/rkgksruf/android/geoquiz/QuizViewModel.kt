65a0cae480f8f57ad53db95aaa536e12fc91c5a4
kahangyeol <rkgksruf321@naver.com>
2023-10-11 23:33:05 +0900

    2023.10.11 add CheatActivity

---
+++ Chapter05/GeoQuiz/app/src/main/java/com/rkgksruf/android/geoquiz/QuizViewModel.kt
@@ -0,0 +1,36 @@
+package com.rkgksruf.android.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+private const val TAG = "QuizViewModel"
+class QuizViewModel: ViewModel() {
+
+    var currentIndex = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    val currentQuestionAnswer: Boolean get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int get() = questionBank[currentIndex].textResId
+
+    fun moveToText() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+/*  -------ViewModel 로그 찍고 디버깅 해보기 좋음
+    init {      // 초기화 블록, 클래스 인스턴스 생성 시
+        Log.d(TAG, "ViewModel instance created")
+    }
+
+    override fun onCleared() {      // 인스턴스 소멸
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }*/
+}
---
+++ Chapter06/GeoQuiz/app/src/main/java/com/rkgksruf/android/geoquiz/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.rkgksruf.android.geoquiz
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate() call")
+        setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0) ?:0
+        quizViewModel.currentIndex = currentIndex
+/*
+        val provider: ViewModelProvider = ViewModelProvider(this)   // 현재 액티비티와 연관된 ViewModelProvider 인스턴스 생성/반환 한다
+        val quizViewModel = provider.get(QuizViewModel::class.java)       // QuizViewModel 인스턴스 반환
+        // ViewModelProvider(this).get(QuizViewModel::class.java) 위에 두줄 이렇게 적어도 됨
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+*/
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        trueButton.setOnClickListener { _ : View ->
+            checkAnswer(true)
+        }
+
+        falseButton.setOnClickListener { _:View ->
+            checkAnswer(false)
+        }
+
+        nextButton.setOnClickListener {
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() call")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() call")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() call")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() call")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() call")
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = quizViewModel.currentQuestionText
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+}
